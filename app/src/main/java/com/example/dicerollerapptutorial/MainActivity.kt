09df33cdea8e348a24feb014a2bbea85ef695b4d
Ryan Aranzamendez <aranzamendeza10@gmail.com>
2022-03-27 23:23:12 -0400

    Added button functionality to display 6 when called -- attempted to add user input for dice sides, failed

--- app/src/main/java/com/example/dicerollerapptutorial/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapptutorial/MainActivity.kt
@@ -4,16 +4,41 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
-        }
+
+
+        rollButton.setOnClickListener { rollDice() }
+}
+
+private fun rollDice(){
+    //val diceChoice = insertNumber()
+    val dice = Dice(6) //TODO: Change from static to user input number, learn what user input command is
+    val diceRoll = dice.roll()
+    val resultTextView: TextView = findViewById(R.id.textView)
+    resultTextView.text = diceRoll.toString()
+}
+
+
+
+class Dice(val numSides: Int) {
+    fun roll(): Int{
+        return(1..numSides).random()
     }
 }
+
+/*fun insertNumber(): Int {
+    val reader = Scanner(System.`in`)
+    print("Please type a number: ")
+    var userNumber: Int = reader.nextInt()
+    return(userNumber)
+}*/
